dea1332f960d72057ab7087ee068a8c037866f6c
aldanaoc <aldanadypw@gmail.com>
2026-07-10 01:34:16 -0300

    const no let u_u

--- js/form.js
+++ js/form.js
@@ -1,23 +1,15 @@
 // arrancamos con lo del formulario
 function mensajeUsuario() {
-    let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    let email =document.getElementById("email").value;
-    let telefono = document.getElementById("telefono").value;
-    let asunto = document.getElementById("asunto").value;
-    let mensaje = document.getElementById("mensaje").value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email =document.getElementById("email").value;
+    const telefono = document.getElementById("telefono").value;
+    const asunto = document.getElementById("asunto").value;
+    const mensaje = document.getElementById("mensaje").value;
         if (nombre === "" || apellido === "" || email === "" || telefono === "" || asunto === "" || mensaje === "") {
             alert("Complete todos los campos, por favor.");
             return;
         }
-    // usamos php para usar lo de abajo
-    let datosUsuario = {nombre: nombre,
-        apellido: apellido,
-        email: email,
-        telefono: telefono,
-        asunto: asunto,
-        mensaje: mensaje
-    };
     alert("¡Consulta enviada! En un momente nos contactamos con vos.");
     document.querySelector("form").reset();
 }
